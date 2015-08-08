b3aa4dda86084ed94dfcf0d06e7f9ab669590308
Kyle Meyer <kyle@kyleam.com>
2015-08-08 19:39:55 -0400

    Bump version

--- org-link-edit.el
+++ org-link-edit.el
@@ -5,7 +5,7 @@
 ;; Author:  Kyle Meyer <kyle@kyleam.com>
 ;; URL: https://github.com/kyleam/org-link-edit
 ;; Keywords: convenience
-;; Version: 0.1.0
+;; Version: 1.0.0
 ;; Package-Requires: ((cl-lib "0.5") (org "8.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
