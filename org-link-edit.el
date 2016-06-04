8271f5bcf126ba0fb9f19ab4530cf1c8d1bf6566
Kyle Meyer <kyle@kyleam.com>
2016-06-04 19:00:10 -0400

    Update header URL

--- org-link-edit.el
+++ org-link-edit.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015 Kyle Meyer <kyle@kyleam.com>
 
 ;; Author:  Kyle Meyer <kyle@kyleam.com>
-;; URL: https://github.com/kyleam/org-link-edit
+;; URL: https://gitlab.com/kyleam/org-link-edit
 ;; Keywords: convenience
 ;; Version: 1.0.1
 ;; Package-Requires: ((cl-lib "0.5") (org "8.2"))
